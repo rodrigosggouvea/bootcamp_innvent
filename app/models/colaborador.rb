e391b93d8e6f2c317fc829f261d2f47a56c20ee0
Rodrigo Gouvêa <rodrigosggouvea@gmail.com>
2013-07-03 14:08:15 -0300

    validacao e testes de colaborador

--- app/models/colaborador.rb
+++ app/models/colaborador.rb
@@ -1,2 +1,9 @@
+#encoding: UTF-8
 class Colaborador < ActiveRecord::Base
+  has_many :projetos, :through => :colaboradores_projetos
+
+  validates :nome,      :presence => {:message => "O nome deve ser preenchido."},      :uniqueness => {:message => "Já existe um colaborador com esse nome."}
+  validates :matricula, :presence => {:message => "A matricula deve ser preenchido."}, :uniqueness => {:message => "Já existe um colaborador com essa matricula."}
+  validates :cargo,     :presence => {:message => "O cargo deve ser preenchido."}
+
 end
--- spec/models/colaborador_spec.rb
+++ spec/models/colaborador_spec.rb
@@ -2,5 +2,33 @@
 require 'spec_helper'
 
 describe 'Colaborador' do
-  pending "Criar definição!"
+  let(:colaborador) {Colaborador.new}
+  subject { colaborador }
+
+  describe "Validações" do
+    context "nome" do
+      it "deve ser preenchido" do
+        colaborador.valid?.should be_false
+        colaborador.errors[:nome].should_not be_empty
+      end
+    end
+    context "cargo" do
+      it "deve ser preenchido" do
+        colaborador.valid?.should be_false
+        colaborador.errors[:cargo].should_not be_empty
+      end
+    end
+    context "matricula" do
+      it "deve ser preenchido" do
+        colaborador.valid?.should be_false
+        colaborador.errors[:matricula].should_not be_empty
+      end
+      it "deve ser único" do
+        Colaborador.create(:nome => "Teste", :cargo => "Teste", :matricula => "teste")
+        colaborador.matricula = "teste"
+        colaborador.valid?.should be_false
+        colaborador.errors[:matricula].should include("Já existe um colaborador com essa matricula.")
+      end
+    end
+  end
 end
